aa2e16b5f4d94447db9070b018f6f378a7c6789a
Bjorn Burton <bjorn@mm.st>
2014-12-28 01:18:56 -0500

    improved comments

--- avrdude.sh
+++ avrdude.sh
@@ -8,14 +8,13 @@
 # like Trinket and Gemma, to be easily supported.
 # It also allows the use of a pre-existing install
 # of Avrdude, perhaps of a newer release, to be used
-# with it's compatible avrdude.conf file. 
+# with its compatible avrdude.conf file. 
 #
 # It must sit in place of the avrdude located in
 # /usr/share/arduino/hardware/tools/
 # Since it must share that directory, the original
-# avrdude should be renamed. The AVRDUDE string may
-# be pointed to the original, if desired.
-# Of course, this file must be named "avrdude" and
+# avrdude should be renamed to "avrdude.old"
+# Of course, this script must be named "avrdude" and
 # be made executable through "chmod +x avrdude"
 ######################################################
 
@@ -27,10 +26,12 @@ BOARD="trinket"
 # (hint: usbtiny is emulated in Trinket and Gemma)
 PROGRAMMER="usbtiny"
 
-# Location of real avrdude (Hint: use no path for default avrdude)
+# Location of better avrdude (Hint: use no path for default avrdude)
+# AVRDUDE=/usr/share/arduino/hardware/tools/avrdude.old
 AVRDUDE="avrdude"
 
-# Matching avrdude.conf file.
+# Matching avrdude.conf file
+# CONF=/usr/share/arduino/hardware/tools/avrdude.conf
 CONF="/etc/avrdude.conf"
 
 # We assemble all of the parameters
